35dfc8c0a1b9455f65f7dfbbf1ae4bea495c3334
LOBSTER10000 <ghh112034@gmail.com>
2024-10-17 23:37:03 +0900

    chatizBack 001 main : start
    start project

---
+++ src/main/java/com/example/chatizback/ChatizBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatizback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatizBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatizBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatizback/ChatizBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.chatizback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatizBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("안녕");
+	}
+
+}
